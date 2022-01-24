6c5ec00aeffa90e07fd2165f6ef1f989119d95fd
Troy Records Jr <giants2110@gmail.com>
2022-01-24 07:22:35 -0500

    Updated to new UE5 version
    Using commit 1fd399e from ue5-main

--- CleanRebuild.bat
+++ CleanRebuild.bat
@@ -5,7 +5,6 @@
 @del /q %cd%\*.sln
 
 set MyUVS="C:\Program Files (x86)\Epic Games\Launcher\Engine\Binaries\Win64\UnrealVersionSelector.exe"
-set MyUBT="C:/UnrealSourceBuilds/UE5Source/Engine/Binaries/DotNET/UnrealBuildTool/UnrealBuildTool.exe"
 
 set MyFullPath="%cd%\UE5Testing"
 
