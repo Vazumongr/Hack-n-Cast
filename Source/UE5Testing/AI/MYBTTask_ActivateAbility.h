6f18b37588efe37b09082c2642b9e9a75d3d03f0
Troy Records Jr <giants2110@gmail.com>
2022-02-25 08:06:47 -0500

    Created ActivatePrimaryAbility BTNode

---
+++ Source/UE5Testing/AI/MYBTTask_ActivateAbility.h
@@ -0,0 +1,23 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "MYBTTask_ActivateAbility.generated.h"
+
+class UBehaviorTree;
+
+UCLASS()
+class UE5TESTING_API UMYBTTask_ActivateAbility : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+	UMYBTTask_ActivateAbility();
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	
+#if WITH_EDITOR
+	virtual FName GetNodeIconName() const override;
+#endif // WITH_EDITOR
+};
