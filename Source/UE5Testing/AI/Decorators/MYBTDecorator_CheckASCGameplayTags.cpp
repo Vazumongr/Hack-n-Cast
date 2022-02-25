e6a4f1195a482556267b8657666e027f1c431287
Troy Records Jr <giants2110@gmail.com>
2022-02-25 11:58:58 -0500

    Added Decorator for checking ASC Gameplay Tags

---
+++ Source/UE5Testing/AI/Decorators/MYBTDecorator_CheckASCGameplayTags.cpp
@@ -0,0 +1,51 @@
+﻿// Troy Records Jr. 2021
+
+
+#include "MYBTDecorator_CheckASCGameplayTags.h"
+
+#include "AbilitySystemInterface.h"
+#include "BehaviorTree/Blackboard/BlackboardKeyType_Object.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "UE5Testing/AbilitySystem/MYAbilitySystemComponent.h"
+
+UMYBTDecorator_CheckASCGameplayTags::UMYBTDecorator_CheckASCGameplayTags()
+{
+	NodeName = "ASCGameplay Tag Condition";
+}
+
+bool UMYBTDecorator_CheckASCGameplayTags::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp,
+                                                                     uint8* NodeMemory) const
+{
+	const UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (BlackboardComp == nullptr)
+	{
+		return false;
+	}
+
+	IAbilitySystemInterface* AbilitySystemInterface = Cast<IAbilitySystemInterface>(BlackboardComp->GetValue<UBlackboardKeyType_Object>(ActorToCheck.GetSelectedKeyID()));
+	if (AbilitySystemInterface == nullptr)
+	{
+		return false;
+	}
+
+	UAbilitySystemComponent* ASC = AbilitySystemInterface->GetAbilitySystemComponent();
+	if(ASC == nullptr)
+	{
+		return false;
+	}
+	
+	switch (TagsToMatch)
+	{
+		case EGameplayContainerMatchType::All:
+			return ASC->HasAllMatchingGameplayTags(GameplayTags);
+
+		case EGameplayContainerMatchType::Any:
+			return ASC->HasAnyMatchingGameplayTags(GameplayTags);
+
+		default:
+			{
+				UE_LOG(LogBehaviorTree, Warning, TEXT("Invalid value for TagsToMatch (EGameplayContainerMatchType) %d.  Should only be Any or All."), static_cast<int32>(TagsToMatch));
+				return false;
+			}
+	}
+}
---
+++ Source/UE5Testing/AI/Decorators/MYBTDecorator_CheckASCGameplayTags.h
@@ -0,0 +1,22 @@
+﻿// Troy Records Jr. 2021
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Decorators/BTDecorator_CheckGameplayTagsOnActor.h"
+
+#include "MYBTDecorator_CheckASCGameplayTags.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5TESTING_API UMYBTDecorator_CheckASCGameplayTags : public UBTDecorator_CheckGameplayTagsOnActor
+{
+public:
+    GENERATED_BODY()
+
+	UMYBTDecorator_CheckASCGameplayTags();
+
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+};
