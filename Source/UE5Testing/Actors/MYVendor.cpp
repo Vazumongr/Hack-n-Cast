5e4c2a1a1df8cde0fc59900c433ef1ca40a92f7b
Troy Records Jr <giants2110@gmail.com>
2022-03-06 18:35:18 -0500

    Created Vendor class

---
+++ Source/UE5Testing/Actors/MYVendor.cpp
@@ -0,0 +1,35 @@
+﻿// Troy Records Jr. 2021
+
+
+#include "MYVendor.h"
+
+DEFINE_LOG_CATEGORY(LogVendor);
+
+// Sets default values
+AMYVendor::AMYVendor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AMYVendor::Activate()
+{
+	SetHidden(false);
+}
+
+void AMYVendor::Deactivate()
+{
+	SetHidden(true);
+}
+
+void AMYVendor::Interact()
+{
+	UE_LOG(LogVendor, Warning, TEXT("Vendor interaction called"));
+}
+
+// Called when the game starts or when spawned
+void AMYVendor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
---
+++ Source/UE5Testing/Actors/MYVendor.h
@@ -0,0 +1,27 @@
+﻿// Troy Records Jr. 2021
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MYVendor.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogVendor, All, All);
+
+UCLASS()
+class UE5TESTING_API AMYVendor : public AStaticMeshActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AMYVendor();
+
+	void Activate();
+	void Deactivate();
+	void Interact();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+};
